736d40c8a08abee8727673b1ac1dc12dbbf20d03
Sareh <43628206+Sarehraofian@users.noreply.github.com>
2020-12-14 02:36:04 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import LoginForm from'./components/LoginForm'
+export default function App() {
+  return (
+    <View style={styles.container}>
+        <LoginForm />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
